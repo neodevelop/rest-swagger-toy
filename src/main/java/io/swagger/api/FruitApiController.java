40ef10ccd19d6a2f51471f78f10b536ee2d85d9d
neodevelop <juan@makingdevs.com>
2017-12-04 13:26:41 -0600

    Creando almacén temporal

--- src/main/java/io/swagger/api/FruitApiController.java
+++ src/main/java/io/swagger/api/FruitApiController.java
@@ -1,43 +1,44 @@
 package io.swagger.api;
 
+import io.swagger.annotations.ApiParam;
 import io.swagger.model.Fruit;
-
-import io.swagger.annotations.*;
-
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.multipart.MultipartFile;
 
-import java.util.List;
-
-import javax.validation.constraints.*;
 import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.List;
 @javax.annotation.Generated(value = "io.swagger.codegen.languages.SpringCodegen", date = "2017-11-29T21:37:44.994Z")
 
 @Controller
 public class FruitApiController implements FruitApi {
 
-
+    final List<Fruit> fruits = new ArrayList<Fruit>();
 
     public ResponseEntity<Void> addFruit(@ApiParam(value = "The fruit" ,required=true )  @Valid @RequestBody Fruit fruit) {
-        // do some magic!
+        fruit.setId(fruits.size() + 1L);
+        fruits.add(fruit);
         return new ResponseEntity<Void>(HttpStatus.OK);
     }
 
     public ResponseEntity<Fruit> fruitGet(@ApiParam(value = "ID of the fruit",required=true ) @PathVariable("fruitId") Integer fruitId) {
-        // do some magic!
-        return new ResponseEntity<Fruit>(HttpStatus.OK);
+        Fruit fruit = fruits.get(fruitId - 1);
+        ResponseEntity.BodyBuilder builder = (ResponseEntity.BodyBuilder) ResponseEntity.ok();
+        builder.allow(HttpMethod.GET);
+        builder.contentType(MediaType.APPLICATION_JSON);
+        return builder.body(fruit);
     }
 
     public ResponseEntity<List<Fruit>> listFruits() {
-        // do some magic!
-        return new ResponseEntity<List<Fruit>>(HttpStatus.OK);
+        ResponseEntity.BodyBuilder builder = (ResponseEntity.BodyBuilder) ResponseEntity.ok();
+        builder.allow(HttpMethod.GET);
+        builder.contentType(MediaType.APPLICATION_JSON);
+        return builder.body(fruits);
     }
 
 }
